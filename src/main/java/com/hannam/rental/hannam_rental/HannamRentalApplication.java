c3f882ab1db6f23f6cb5af702e1d73b664e6b856
eongeung <eongeung1022@gmail.com>
2025-02-14 16:55:50 +0900

    스프링 부트 업데이트

---
+++ src/main/java/com/hannam/rental/hannam_rental/HannamRentalApplication.java
@@ -0,0 +1,14 @@
+package com.hannam.rental.hannam_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HannamRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HannamRentalApplication.class, args);
+		System.out.println("hi~ hannam-rental!!!!!");
+	}
+
+}
---
+++ src/test/java/com/hannam/rental/hannam_rental/HannamRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hannam.rental.hannam_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HannamRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
