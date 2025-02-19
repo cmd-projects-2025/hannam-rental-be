70936c918f4ffcfb0490195c45bafb4437f1a8e7
eongeung <eongeung1022@gmail.com>
2025-02-19 13:27:11 +0900

    feat : UserDto 클래스 생성

---
+++ src/main/java/com/hannam/rental/hannam_rental/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.hannam.rental.hannam_rental.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+    private String password;
+    private String name;
+    private String phoneNumber;
+}
