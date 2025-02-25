e1e98adbfca7d82e9809ef2d9be4976b295341c1
jangjuntae <juntae0629@naver.com>
2025-02-25 22:39:33 +0900

    ProductEntity 생성

---
+++ src/main/java/com/hannam/rental/hannam_rental/dto/MyPageRequest.java
@@ -0,0 +1,8 @@
+package com.hannam.rental.hannam_rental.dto;
+
+public class MyPageRequest {
+    private String name;
+    private String product;
+    private String rentalDate;
+    private String retrieve;
+}
