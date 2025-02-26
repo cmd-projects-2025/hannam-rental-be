0ba4989c4cd8897ca12667bc044e824e7c6141b5
eongeung <eongeung1022@gmail.com>
2025-02-26 16:54:41 +0900

    feat: 주석 추가

--- src/main/java/com/hannam/rental/hannam_rental/repository/UserRepository.java
+++ src/main/java/com/hannam/rental/hannam_rental/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.hannam.rental.hannam_rental.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+//회원정보 조회
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByStudentId(String studentId);
     Optional<User> findByPhoneNumber(String phoneNumber);
