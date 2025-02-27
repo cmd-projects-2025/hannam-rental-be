221ddff05f41b127521949759036a6db10801fe2
eongeung <eongeung1022@gmail.com>
2025-02-28 04:39:40 +0900

    feat: User fix

--- src/main/java/com/hannam/rental/hannam_rental/entity/User.java
+++ src/main/java/com/hannam/rental/hannam_rental/entity/User.java
@@ -1,13 +1,7 @@
 package com.hannam.rental.hannam_rental.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
